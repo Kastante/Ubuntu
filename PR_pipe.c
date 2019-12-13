9d4bede4eb18ccb52806ee647bd272d883036215
Kastante <37043087+Kastante@users.noreply.github.com>
2019-12-14 00:56:49 +0300

    Update PR_pipe.c

--- PR_pipe.c
+++ PR_pipe.c
@@ -1,40 +1,28 @@
-main(int argc, char *argv[])
-       {
-           int pipefd[2];
-           pid_t cpid;
-           char buf;
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
 
-           if (argc != 2) {
-               fprintf(stderr, "Usage: %s <string>\n", argv[0]);
-               exit(EXIT_FAILURE);
-           }
-
-           if (pipe(pipefd) == -1) {
-               perror("pipe");
-               exit(EXIT_FAILURE);
-           }
-
-           cpid = fork();
-           if (cpid == -1) {
-               perror("fork");
-               exit(EXIT_FAILURE);
-           }
-
-           if (cpid == 0) {    /* Child reads from pipe */
-               close(pipefd[1]);          /* Close unused write end */
-
-               while (read(pipefd[0], &buf, 1) > 0)
-                   write(STDOUT_FILENO, &buf, 1);
-
-               write(STDOUT_FILENO, "\n", 1);
-               close(pipefd[0]);
-               _exit(EXIT_SUCCESS);
-
-           } else {            /* Parent writes argv[1] to pipe */
-               close(pipefd[0]);          /* Close unused read end */
-               write(pipefd[1], argv[1], strlen(argv[1]));
-               close(pipefd[1]);          /* Reader will see EOF */
-               wait(NULL);                /* Wait for child */
-               exit(EXIT_SUCCESS);
-           }
-       }
+int main (int argc, char * argv[])
+{
+  int pipedes[2];
+  pid_t pid;
+  pipe(pipedes);
+  pid = fork();
+  if ( pid > 0 )  
+  {  
+    char  *str = "String passed via pipe\n";
+    close(pipedes[0]);
+    write(pipedes[1], (void *) str, strlen(str) + 1); 
+    close(pipedes[1]);
+  }  
+  else
+  {
+    char buf[1024];
+    int len;
+    close(pipedes[1]);                                         //close input закрытие передачи в pipe     
+    while ((len = read(pipedes[0], buf, 1024)) != 0)           //
+    write(2, buf, len); 
+    close(pipedes[0]);	
+  }
+  return 0;
+}
